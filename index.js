c5e54ef95d574b775b46180723f73578da752a85
Alexander Zhelyazov <git@zhlyzv.xyz>
2020-01-29 17:47:58 +0000

    add tests

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ const isString = string => Object.prototype.toString.call(string) === '[object S
  * // Click <a href="https://google.com">here.</a>
  * @param {String} string The string you want to format
  * @param {...(String|React.Component)} args Replacement values for the string placeholders({0}, {1}, ..)
+ * @returns {(String|React.Component)[]}
  */
 function formattedString(string, ...args) {
-    if (string === '') {
-        return string;
-    }
     if (!string || !isString(string)) {
         throw new TypeError('Type mismatch: provided argument is not a string.');
+    }
+    if (string === '' || args.length === 0) {
+        return string;
     }
     const replaceableMatches = getReplaceableMatches(string);
     if (!replaceableMatches) return string;
---
+++ index.test.js
@@ -0,0 +1,31 @@
+const formattedString = require('./index');
+
+const testString = 'Hello {0}';
+
+test('Returns single string replacement', () => {
+    const result = formattedString(testString, 'world!');
+    expect(result).toContain('world!');
+});
+
+test('Returns multiple string replacements', () => {
+    const result = formattedString(
+        'The {0} brown {1} jumps over the lazy {2}.',
+        'quick',
+        'fox',
+        'dog'
+    );
+    expect(result).toContain('quick');
+    expect(result).toContain('fox');
+    expect(result).toContain('dog');
+});
+
+test('Returns initial value with no arguments provided', () => {
+    const result = formattedString(testString);
+    expect(result).toEqual(testString);
+});
+
+test('Throws an error if first argument is not a string', () => {
+    expect(() => {
+        formattedString({ foo: 'bar' });
+    }).toThrowError();
+});
